8cf032069a60452ce069f7c4c37d8141135b447e
Tarunkrishna Ramji <tramji3@gatech.edu>
2024-07-27 11:35:12 -0500

    styling game

--- pokemon-hangman/src/components/Game/Game.jsx
+++ pokemon-hangman/src/components/Game/Game.jsx
@@ -8,36 +8,43 @@ function Game() {
     const [poke, setPoke] = useState('')
     const [guessesLeft, setGuessesLeft] = useState(6);
     const [guessedLetters, setGuessedLetters] = useState([]);
+    const [gameOver, setGameOver] = useState(false);
+    const [gameWon, setGameWon] = useState(false)
 
     useEffect(() => {
         async function getPoke() {
-          const pokemonName = await randomPoke();
-          setPoke(pokemonName);
+            if (!gameOver) {
+                const pokemonName = await randomPoke();
+                setPoke(pokemonName);
+            }
         }
         getPoke();
-    }, []);
+    }, [gameOver]);
 
     const word = poke.split("").map(letter => {
-        return guessedLetters.includes(letter) ? letter : '_';
+        return guessedLetters.includes(letter) ? letter.toUpperCase() : '_';
     })
 
     let pokeballs = [];
     for (let i = 0; i < 6; i++) {
-        pokeballs.push(<img src={i < guessesLeft ? pokeball : pokeballLight}></img>)
+        pokeballs.push(<img src={i < guessesLeft ? pokeball : pokeballLight} style={
+            {width: '20px', height: '20px', margin: '1px'}}></img>)
     }
 
-    const buttons = "abcdefghijklmnopqrstuvwxyz".split("").map(letter => {
+    const buttons = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map(letter => {
         return <button 
                     key={letter}
-                    onClick={() => handleGuess(letter)}
-                    disabled={guessedLetters.includes(letter)}
+                    onClick={() => handleGuess(letter.toLowerCase())}
                     style={{ 
-                        backgroundColor: guessedLetters.includes(letter)
-                            ? (poke.includes(letter) ? 'green' : 'red')
-                            : ''
+                        backgroundColor: guessedLetters.includes(letter.toLowerCase())
+                            ? (poke.includes(letter.toLowerCase()) ? '#98fb98' : '#ff9999')
+                            : '',
+                        width: '35px',
+                        height: '35px',
+                        margin: '5px'
                     }}
                 >
-                    {letter}
+                    <strong>{letter}</strong>
                 </button>
     })
 
@@ -48,13 +55,31 @@ function Game() {
                 setGuessesLeft(prev => prev - 1)
             }
         }
+        checkGameOver();
+    }
+
+    function checkGameOver() {
+        if (guessesLeft === 0) {
+            setGameOver(true);
+        }
+        if (guessedLetters.length === poke.length) {
+            setGameOver(true);
+            setGameWon(true);
+        }
+    }
+
+    function resetGame() {
+        setGuessedLetters([]);
+        setGuessesLeft(6);
+        setGameOver(false);
+        setGameWon(false);
     }
 
     return (
-        <div className="box">
-            {word}
-            {pokeballs}
-            {buttons}
+        <div className="box-left">
+            <div className="word"><strong>{word}</strong></div>
+            <div className="pokeballs">{pokeballs}</div>
+            <div className="letters">{buttons}</div>
         </div>
     )
 }
