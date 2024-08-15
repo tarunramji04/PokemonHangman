d35503e5fe13d41749d36dc960e856bff6325b51
Tarunkrishna Ramji <tramji3@gatech.edu>
2024-08-15 16:23:28 -0500

    added basic input validation

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -10,7 +10,9 @@ function Login({isOpen, onRequestClose, onLogin, onPlayAsGuest}) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [createUsername, setCreateUserName] = useState('');
-    const [createPassowrd, setCreatePassword] = useState('');
+    const [createPassword, setCreatePassword] = useState('');
+    const [loginErrorMessage, setLoginErrorMessage] = useState('');
+    const [createErrorMessage, setCreateErrorMessage] = useState('');
 
     useEffect(() => {
         if (isOpen) {
@@ -27,17 +29,26 @@ function Login({isOpen, onRequestClose, onLogin, onPlayAsGuest}) {
             console.log('Logging in with', username, password);
             onLogin(username);
         } catch(error) {
-            console.log(error);
+            setLoginErrorMessage('Invalid username or password');
         }
     };
   
     async function handleCreateAccount(event) {
         event.preventDefault();
-        const {token} = await createUser(createUsername, createPassowrd);
-        localStorage.setItem("token", token);
-        console.log('Created account with', createUsername, createPassowrd);
-        //creating account auto logs in user
-        onLogin(createUsername);
+        try {
+            if (createUsername.length >= 6 && createPassword.length >= 6) {
+                const {token} = await createUser(createUsername, createPassword);
+                localStorage.setItem("token", token);
+                console.log('Created account with', createUsername, createPassword);
+                //creating account auto logs in user
+                onLogin(createUsername);
+            } else {
+                setCreateErrorMessage('Username and password must be at least 6 characters');
+            }
+            
+        } catch(error) {
+            setCreateErrorMessage('Username already taken');
+        }
     };
 
     function resetInputs() {
@@ -45,6 +56,8 @@ function Login({isOpen, onRequestClose, onLogin, onPlayAsGuest}) {
         setPassword('');
         setCreateUserName('');
         setCreatePassword('');
+        setLoginErrorMessage('');
+        setCreateErrorMessage('');
         setShowCreateAccount(false);
     }
 
@@ -74,7 +87,7 @@ function Login({isOpen, onRequestClose, onLogin, onPlayAsGuest}) {
                 }
             }}>
             {!showCreateAccount ? (
-            <div className="modal-content" style={{height: '440px'}}>
+            <div className="modal-content" style={{height: loginErrorMessage ? '456px' : '440px'}}>
                 <div className='modal-welcome'>
                     <h1>Welcome to Pokemon Hangman</h1>
                     <div>{pokeballs}</div>
@@ -94,15 +107,25 @@ function Login({isOpen, onRequestClose, onLogin, onPlayAsGuest}) {
                         onChange={(e) => setPassword(e.target.value)}
                     />
                     <button type="submit"><strong>LOGIN</strong></button>
+                    {loginErrorMessage && (
+                        <div className="error-message" style={{color: 'red', fontSize: '0.8rem'}}>
+                            {loginErrorMessage}
+                        </div>
+                    )}
                 </form>
                 <div className='bottom'>
                     <div><strong>Don't have an account?</strong></div>
-                    <button onClick={() => setShowCreateAccount(true)}><strong>CREATE ACCOUNT</strong></button>
+                    <button onClick={() => {
+                        setShowCreateAccount(true)
+                        setLoginErrorMessage('')
+                    }}><strong>CREATE ACCOUNT</strong></button>
                     <button onClick={onPlayAsGuest}><strong>PLAY AS GUEST</strong></button>
                 </div>
             </div>
             ) : (
-            <div className="modal-content" style={{height: '180px'}}>
+            <div className="modal-content" style={{
+                height: createErrorMessage ? createErrorMessage === 'Username already taken' ? '192px' : '205px' : '180px'
+            }}>
                 <h2><strong>CREATE ACCOUNT</strong></h2>
                 <form onSubmit={handleCreateAccount}>
                     <input
@@ -114,12 +137,20 @@ function Login({isOpen, onRequestClose, onLogin, onPlayAsGuest}) {
                     <input
                         type="password"
                         placeholder="Password"
-                        value={createPassowrd}
+                        value={createPassword}
                         onChange={(e) => setCreatePassword(e.target.value)}
                     />
                     <button type="submit"><strong>CREATE ACCOUNT</strong></button>
+                    {createErrorMessage && (
+                        <div className="error-message" style={{color: 'red', fontSize: '0.8rem'}}>
+                            {createErrorMessage}
+                        </div>
+                    )}
                 </form>
-                <button onClick={() => setShowCreateAccount(false)}><strong>BACK</strong></button>
+                <button onClick={() => {
+                    setShowCreateAccount(false)
+                    setCreateErrorMessage('')
+                }}><strong>BACK</strong></button>
             </div>
             )}
         </Modal>
