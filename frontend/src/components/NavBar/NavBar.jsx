6213db71b53b36a094e0f8d900b4f4c0ca598a51
Tarunkrishna Ramji <tramji3@gatech.edu>
2024-08-15 15:40:50 -0500

    fixed dropdown closing when screen is clicked anywhere

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import Modal from 'react-modal';
+import { useState, useEffect, useRef } from 'react'
 import Pokedex from "../Pokedex/Pokedex"
 import './style.css'
 
 function NavBar({user, isLoggedIn, onClickLogIn, onClickLogOut, guessed}) {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isPokedexOpen, setIsPokedexOpen] = useState(false);
+    const menuRef = useRef(null);
+    const buttonRef = useRef(null);
 
     function toggleMenu() {
         setIsMenuOpen(!isMenuOpen);
@@ -23,10 +24,23 @@ function NavBar({user, isLoggedIn, onClickLogIn, onClickLogOut, guessed}) {
         onClickLogOut();
     }
 
+    function handleClickOutside(event) {
+        if (menuRef.current && !menuRef.current.contains(event.target) && buttonRef.current && !buttonRef.current.contains(event.target)) {
+            setIsMenuOpen(false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [])
+
     return (
         <div className="nav-bar">
             {isLoggedIn ? (
-                <div className="user-circle" onClick={toggleMenu}>
+                <div className="user-circle" onClick={toggleMenu} ref={buttonRef}>
                     <strong>{user.charAt(0)}</strong>
                 </div>
             ) : (
@@ -35,7 +49,7 @@ function NavBar({user, isLoggedIn, onClickLogIn, onClickLogOut, guessed}) {
                 </div>
             )}
             {isMenuOpen && (
-                <div className="dropdown-menu">
+                <div className="dropdown-menu" ref={menuRef}>
                     <div className="dropdown-item" onClick={togglePokedex}>
                         <strong>Pokedex</strong>
                     </div>
